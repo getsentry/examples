63e0a1a49b673aa3a29a5f80bfddb0de93a48fa0
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-07-19 10:25:35 +0200

    Inital commit, Add cocoa example, react-native example

---
+++ cocoa/sentry-ios-cocoapods/sentry-ios-cocoapods/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  sentry-ios-cocoapods
+//
+//  Created by Daniel Griesser on 18.07.17.
+//  Copyright © 2017 Sentry. All rights reserved.
+//
+
+import UIKit
+import Sentry
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do {
+            Client.shared = try Client(dsn: "https://663998f40e734ea59087883feda37647:306481b9f6bb4a6287b334178d9f8c71@sentry.io/4394")
+            try Client.shared?.startCrashHandler()
+        } catch let error {
+            print("\(error)")
+            // Wrong DSN or KSCrash not installed
+        }
+        Client.shared?.enableAutomaticBreadcrumbTracking()
+    }
+
+    @IBAction func sendMessage(_ sender: Any) {
+        let event = Event(level: .debug)
+        event.message = "Test Message"
+        Client.shared?.send(event: event) { (error) in
+            // Optional callback after event has been send
+        }
+    }
+    
+    @IBAction func causeCrash(_ sender: Any) {
+        Client.shared?.crash()
+    }
+    
+}
+
---
+++ react-native/README.md
@@ -0,0 +1,7 @@
+# Requirements
+
+`node, yarn`
+
+# Run
+
+Just open the `AwesomeProject` folder and call `react-native run-ios`.
