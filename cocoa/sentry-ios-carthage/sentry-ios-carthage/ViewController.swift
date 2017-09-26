21a02c36ddab9bce89b63a69689843436262189f
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-09-26 14:18:06 +0200

    feat: Add carthage example project

---
+++ cocoa/sentry-ios-carthage/sentry-ios-carthage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sentry-ios-carthage
+//
+//  Created by Daniel Griesser on 26.09.17.
+//  Copyright © 2017 Sentry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
