c8d991c6b5a87e17196952661af71a09dc313f6a
Neil Manvar <ndmanvar@yahoo.com>
2017-10-09 18:59:37 -0700

    remove project_id

--- node/example.js
+++ node/example.js
@@ -1,6 +1,6 @@
 var Raven = require('raven');
 
-Raven.config('https://<PUBLIC_DSN_KEY>:<PRIVATE_DSN_KEY>@sentry.io/227943').install();
+Raven.config('https://<PUBLIC_DSN_KEY>:<PRIVATE_DSN_KEY>@sentry.io/<PROJECT_ID>').install();
 
 var x = {};
 x.something(); // this error should be shot up to Sentry
