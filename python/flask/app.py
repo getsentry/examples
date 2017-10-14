9eb2a3a4bd1b3297866487ee86282383b89f40bf
Chloe Condon <misschloecondon@gmail.com>
2017-10-14 14:03:41 -0700

    Changed hello error to hello world and removed duplicate html file

--- python/flask/app.py
+++ python/flask/app.py
@@ -11,7 +11,7 @@ sentry = Sentry(app, dsn='https://<PUBLIC_DSN_KEY>:<PRIVATE_DSN_KEY>@sentry.io/<
 def hello_error():
 
     1 / 0 #ZeroDivisionError to be sent to sentry
-    return render_template("hello_error.html")
+    return render_template("hello_world.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
