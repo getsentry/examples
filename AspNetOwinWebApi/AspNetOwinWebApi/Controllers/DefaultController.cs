296e2778930511d8dc60f10229716b719e17b510
Bruno Garcia <bruno@brunogarcia.com>
2019-07-19 18:09:08 +0200

    new project

---
+++ AspNetOwinWebApi/AspNetOwinWebApi/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace AspNetOwinWebApi.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ AspNetOwinWebApi/AspNetOwinWebApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace AspNetOwinWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
