9e811d71f79166744723b3a718e47cf344a4d0c2
Klemens Mantzos <klemens@fetzig.at>
2019-11-27 13:14:47 +0100

    adds tvOS example

---
+++ cocoa/sentry-tvos-obj-c-cocoapods/sentry-tvos-obj-c-cocoapods/AppDelegate.m
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.m
+//  sentry-tvos-obj-c-cocoapods
+//
+//  Created by Klemens Mantzos on 27.11.19.
+//  Copyright © 2019 Sentry. All rights reserved.
+//
+
+#import "AppDelegate.h"
+@import Sentry;
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+
+    NSError *error = nil;
+    SentryClient *client = [[SentryClient alloc] initWithDsn:@"https://663998f40e734ea59087883feda37647@sentry.io/4394" didFailWithError:&error];
+    SentryClient.sharedClient = client;
+    [SentryClient.sharedClient startCrashHandlerWithError:&error];
+    if (nil != error) {
+        NSLog(@"%@", error);
+    }
+
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+@end
---
+++ cocoa/sentry-tvos-obj-c-cocoapods/sentry-tvos-obj-c-cocoapods/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  sentry-tvos-obj-c-cocoapods
+//
+//  Created by Klemens Mantzos on 27.11.19.
+//  Copyright © 2019 Sentry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
