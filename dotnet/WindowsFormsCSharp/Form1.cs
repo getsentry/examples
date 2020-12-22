36721d7c55ee4273df2ef95573af9f80f3841bcd
Bruno Garcia <bruno@brunogarcia.com>
2020-12-21 21:29:37 -0500

    winforms (#66)

---
+++ dotnet/WindowsFormsCSharp/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using Sentry;
+
+namespace WindowsFormsCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SentrySdk.AddBreadcrumb($"{nameof(button1_Click)} clicked.", "ui.lifecycle");
+
+            throw null;
+        }
+    }
+}
