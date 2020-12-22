fa3ccbb8098f57a394240c2a0ef82a2e362c1cbf
Bruno Garcia <bruno@brunogarcia.com>
2020-12-21 20:55:57 -0500

    wpf sample

---
+++ dotnet/WpfDotNetCoreCSharp/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Sentry;
+
+namespace WpfDotNetCoreCSharp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SentrySdk.AddBreadcrumb($"{nameof(Button_Click)} clicked.", "ui.lifecycle");
+
+            throw null;
+        }
+    }
+}
