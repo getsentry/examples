41fc9086db9d71130c0bcdc84143ff12742e504d
Bruno Garcia <bruno@brunogarcia.com>
2020-12-21 20:58:06 -0500

    wpf sample (#65)

---
+++ dotnet/WpfDotNetCoreCSharp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using Sentry;
+
+namespace WpfDotNetCoreCSharp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            // NOTE: Change the URL below to your own DSN. Get it on sentry.io in the project settings (or when you create a new project):
+            SentrySdk.Init("https://80aed643f81249d4bed3e30687b310ab@o447951.ingest.sentry.io/5428537");
+        }
+        
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            SentrySdk.CaptureException(e.Exception);
+
+            // If you want to stop the application from crashing, uncomment this line:
+            // e.Handled = true;
+        }
+    }
+}
---
+++ dotnet/WpfDotNetCoreCSharp/README.md
@@ -0,0 +1,11 @@
+# Sentry for Windows Presentation Foundation
+
+This example shows how to initalize Sentry and capture a crash of the WPF app.
+
+Make sure to add [your own DSN](https://docs.sentry.io/product/sentry-basics/dsn-explainer/) to [App.xaml.cs](App.xaml.cs) to see the events in your own Sentry project.
+
+![WPF sample](wpf.png)
+
+## Next steps
+
+Check out the [Sentry docs for more options of the .NET and WPF SDKs.(https://docs.sentry.io/platforms/dotnet/guides/winforms/) 
