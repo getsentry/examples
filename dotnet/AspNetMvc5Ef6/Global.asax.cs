e71a4bd0f381414aaa33caa9308a57071334c4b3
Tyrrrz <tyrrrz@gmail.com>
2021-04-15 21:54:19 +0300

    wip

--- dotnet/AspNetMvc5Ef6/Global.asax.cs
+++ dotnet/AspNetMvc5Ef6/Global.asax.cs
@@ -55,19 +55,45 @@ namespace AspNetMvc5Ef6
             var method = Context.Request.HttpMethod;
             var path = Context.Request.Path;
 
-            // Start a transaction that wraps the current request and bind it to the scope
+            // Start a transaction that encompasses the current request
             var transaction = SentrySdk.StartTransaction(
                 $"{method} {path}",
                 "http.server"
             );
 
+            // Attach the transaction to the scope so that other operations can reference it
             SentrySdk.ConfigureScope(scope => scope.Transaction = transaction);
+
+            // Attach transaction to the request context to finish it when the request ends
+            Context.Items["__SentryTransaction"] = transaction;
         }
 
         protected void Application_EndRequest()
         {
-            // Finish the currently active span
-            SentrySdk.GetSpan()?.Finish();
+            // Finish the currently active transaction
+            if (Context.Items.Contains("__SentryTransaction"))
+            {
+                var transaction = Context.Items["__SentryTransaction"] as ISpan;
+
+                var status =
+                    Context.Response.StatusCode < 400 ? SpanStatus.Ok :
+                    Context.Response.StatusCode == 400 ? SpanStatus.InvalidArgument :
+                    Context.Response.StatusCode == 401 ? SpanStatus.Unauthenticated :
+                    Context.Response.StatusCode == 403 ? SpanStatus.PermissionDenied :
+                    Context.Response.StatusCode == 404 ? SpanStatus.NotFound :
+                    Context.Response.StatusCode == 409 ? SpanStatus.AlreadyExists :
+                    Context.Response.StatusCode == 429 ? SpanStatus.ResourceExhausted :
+                    Context.Response.StatusCode == 499 ? SpanStatus.Cancelled :
+                    Context.Response.StatusCode < 500 ? SpanStatus.InvalidArgument :
+                    Context.Response.StatusCode == 500 ? SpanStatus.InternalError :
+                    Context.Response.StatusCode == 501 ? SpanStatus.Unimplemented :
+                    Context.Response.StatusCode == 503 ? SpanStatus.Unavailable :
+                    Context.Response.StatusCode == 504 ? SpanStatus.DeadlineExceeded :
+                    Context.Response.StatusCode < 600 ? SpanStatus.InternalError :
+                    SpanStatus.UnknownError;
+
+                transaction?.Finish(status);
+            }
         }
     }
 }
