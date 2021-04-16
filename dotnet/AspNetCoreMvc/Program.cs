e7e9454cdf113b03fcf9e278ecc0bc22f92071a8
Alexey Golub <tyrrrrrr@gmail.com>
2021-04-16 04:09:26 +0300

    Update .NET examples (#77)

--- dotnet/AspNetCoreMvc/Program.cs
+++ dotnet/AspNetCoreMvc/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Net;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Sentry.Samples.AspNetCore.Mvc
@@ -10,38 +10,40 @@ namespace Sentry.Samples.AspNetCore.Mvc
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-
-                // Example integration with advanced configuration scenarios:
-                .UseSentry(o =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    // The parameter 'o' here has values populated through the configuration system.
-                    // That includes 'appsettings.json', environment variables and anything else defined on the ConfigurationBuilder.
-                    // See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.1&tabs=basicconfiguration
+                    webBuilder.UseStartup<Startup>();
 
-                    // Tracks the release which sent the event and enables more features: https://docs.sentry.io/learn/releases/
-                    // If not explicitly set here, the SDK attempts to read it from: AssemblyInformationalVersionAttribute and AssemblyVersion
-                    // TeamCity: %build.vcs.number%, VSTS: BUILD_SOURCEVERSION, Travis-CI: TRAVIS_COMMIT, AppVeyor: APPVEYOR_REPO_COMMIT, CircleCI: CIRCLE_SHA1
-                    // o.Release = "b2946b7e477e1b63dd24db3db4bfdd602e302eec"; // Could be also the be like: 2.0 or however your version your app
+                    // Example integration with advanced configuration scenarios:
+                    webBuilder.UseSentry(o =>
+                    {
+                        // The parameter 'o' here has values populated through the configuration system.
+                        // That includes 'appsettings.json', environment variables and anything else defined on the ConfigurationBuilder.
+                        // See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.1&tabs=basicconfiguration
 
-                    o.MaxBreadcrumbs = 200;
+                        // Tracks the release which sent the event and enables more features: https://docs.sentry.io/learn/releases/
+                        // If not explicitly set here, the SDK attempts to read it from: AssemblyInformationalVersionAttribute and AssemblyVersion
+                        // TeamCity: %build.vcs.number%, VSTS: BUILD_SOURCEVERSION, Travis-CI: TRAVIS_COMMIT, AppVeyor: APPVEYOR_REPO_COMMIT, CircleCI: CIRCLE_SHA1
+                        // o.Release = "b2946b7e477e1b63dd24db3db4bfdd602e302eec"; // Could be also the be like: 2.0 or however your version your app
 
-                    //o.HttpProxy = new WebProxy("https://localhost:3128");
+                        o.MaxBreadcrumbs = 200;
 
-                    // Example: Disabling support to compressed responses:
-                    o.DecompressionMethods = DecompressionMethods.None;
+                        //o.HttpProxy = new WebProxy("https://localhost:3128");
 
-                    o.MaxQueueItems = 100;
-                    o.ShutdownTimeout = TimeSpan.FromSeconds(5);
+                        // Example: Disabling support to compressed responses:
+                        o.DecompressionMethods = DecompressionMethods.None;
 
-                    // Hard-coding here will override any value set on appsettings.json:
-                    o.MinimumEventLevel = LogLevel.Error;
-                })
-                .Build();
+                        o.MaxQueueItems = 100;
+                        o.ShutdownTimeout = TimeSpan.FromSeconds(5);
+
+                        // Hard-coding here will override any value set on appsettings.json:
+                        o.MinimumEventLevel = LogLevel.Error;
+                    });
+                });
     }
 }
--- dotnet/AspNetMvc5Ef6/Global.asax.cs
+++ dotnet/AspNetMvc5Ef6/Global.asax.cs
@@ -4,6 +4,7 @@ using Sentry.EntityFramework;
 using System.Configuration;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Sentry.AspNet;
 
 namespace AspNetMvc5Ef6
 {
@@ -24,7 +25,10 @@ namespace AspNetMvc5Ef6
             _sentry = SentrySdk.Init(o =>
             {
                 // We store the DSN inside Web.config; make sure to use your own DSN!
-                o.Dsn = new Dsn(ConfigurationManager.AppSettings["SentryDsn"]);
+                o.Dsn = ConfigurationManager.AppSettings["SentryDsn"];
+
+                // Get ASP.NET integration
+                o.AddAspNet();
 
                 // Get Entity Framework integration
                 o.AddEntityFramework();
@@ -44,6 +48,52 @@ namespace AspNetMvc5Ef6
         {
             // Close the Sentry SDK (flushes queued events to Sentry)
             _sentry?.Dispose();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            var method = Context.Request.HttpMethod;
+            var path = Context.Request.Path;
+
+            // Start a transaction that encompasses the current request
+            var transaction = SentrySdk.StartTransaction(
+                $"{method} {path}",
+                "http.server"
+            );
+
+            // Attach the transaction to the scope so that other operations can reference it
+            SentrySdk.ConfigureScope(scope => scope.Transaction = transaction);
+
+            // Attach transaction to the request context to finish it when the request ends
+            Context.Items["__SentryTransaction"] = transaction;
+        }
+
+        protected void Application_EndRequest()
+        {
+            // Finish the currently active transaction
+            if (Context.Items.Contains("__SentryTransaction"))
+            {
+                var transaction = Context.Items["__SentryTransaction"] as ISpan;
+
+                var status =
+                    Context.Response.StatusCode < 400 ? SpanStatus.Ok :
+                    Context.Response.StatusCode == 400 ? SpanStatus.InvalidArgument :
+                    Context.Response.StatusCode == 401 ? SpanStatus.Unauthenticated :
+                    Context.Response.StatusCode == 403 ? SpanStatus.PermissionDenied :
+                    Context.Response.StatusCode == 404 ? SpanStatus.NotFound :
+                    Context.Response.StatusCode == 409 ? SpanStatus.AlreadyExists :
+                    Context.Response.StatusCode == 429 ? SpanStatus.ResourceExhausted :
+                    Context.Response.StatusCode == 499 ? SpanStatus.Cancelled :
+                    Context.Response.StatusCode < 500 ? SpanStatus.InvalidArgument :
+                    Context.Response.StatusCode == 500 ? SpanStatus.InternalError :
+                    Context.Response.StatusCode == 501 ? SpanStatus.Unimplemented :
+                    Context.Response.StatusCode == 503 ? SpanStatus.Unavailable :
+                    Context.Response.StatusCode == 504 ? SpanStatus.DeadlineExceeded :
+                    Context.Response.StatusCode < 600 ? SpanStatus.InternalError :
+                    SpanStatus.UnknownError;
+
+                transaction?.Finish(status);
+            }
         }
     }
 }
