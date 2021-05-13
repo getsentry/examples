1dd36845cfd6dea7772e70098e020ac2320f8e90
Tyrrrz <tyrrrrrr@gmail.com>
2021-05-13 23:10:14 +0300

    Simplify

--- dotnet/AspNetMvc5Ef6/Global.asax.cs
+++ dotnet/AspNetMvc5Ef6/Global.asax.cs
@@ -51,36 +51,10 @@ namespace AspNetMvc5Ef6
             _sentry?.Dispose();
         }
 
-        private static SentryTraceHeader TryGetTraceHeader(NameValueCollection headers)
-        {
-            try
-            {
-                var traceHeader = headers.Get("sentry-trace");
-                return SentryTraceHeader.Parse(traceHeader);
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
         protected void Application_BeginRequest()
         {
-            var method = Context.Request.HttpMethod;
-            var path = Context.Request.Path;
-
-            var traceHeader = TryGetTraceHeader(Context.Request.Headers);
-            var transactionName = $"{method} {path}";
-            var transactionOperation = "http.server";
-            var transactionContext = traceHeader != null
-                ? new TransactionContext(transactionName, transactionOperation, traceHeader)
-                : new TransactionContext(transactionName, transactionOperation);
-
             // Start a transaction that encompasses the current request
-            var transaction = SentrySdk.StartTransaction(transactionContext);
-
-            // Attach the transaction to the scope so that other operations can reference it
-            SentrySdk.ConfigureScope(scope => scope.Transaction = transaction);
+            var transaction = Context.StartSentryTransaction();
 
             // Attach transaction to the request context to finish it when the request ends
             Context.Items["__SentryTransaction"] = transaction;
