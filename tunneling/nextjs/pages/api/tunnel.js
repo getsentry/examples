5d2af3b5ef40fd03766ba5375e272e5f16854115
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-07-20 15:14:41 +0200

    chore: change error 200 -> 400

--- tunneling/nextjs/pages/api/tunnel.js
+++ tunneling/nextjs/pages/api/tunnel.js
@@ -14,15 +14,15 @@ async function handler(req, res) {
 
   if (header.dsn) {
     const { host, path } = url.parse(header.dsn);
-    const projectId = path.endsWith('/') ? path.slice(0, -1) : path;
+    const projectId = path.endsWith("/") ? path.slice(0, -1) : path;
     const sentryApi = `https://${host}/api/${projectId}/envelope/`;
-    
+
     const response = await axios.post(sentryApi, envelope);
-    
+
     return res.status(200).json(response.data);
   }
 
-  res.status(200).json({ status: "missing_dsn" });
+  res.status(400).json({ status: "missing_dsn" });
 }
 
 export default withSentry(handler);
