a9f50106988ce2d21e41ab8584ebaccef4988709
Kamil Ogórek <kamil.ogorek@gmail.com>
2021-09-14 11:23:50 +0200

    ref(tunnel): Remove Axios in favor of built-in Fetch in Next.js
    Starting Next.js v9.4, Fetch API is polyfilled in both, Browser and
    Node.js environments
    https://nextjs.org/blog/next-9-4#improved-built-in-fetch-support

--- tunneling/nextjs/pages/api/tunnel.js
+++ tunneling/nextjs/pages/api/tunnel.js
@@ -1,5 +1,4 @@
 import { withSentry, captureException } from "@sentry/nextjs";
-import axios from "axios";
 import * as url from "url";
 
 // Change host appropriately if you run your own Sentry instance.
@@ -13,21 +12,25 @@ async function handler(req, res) {
   try {
     const envelope = req.body;
     const pieces = envelope.split("\n");
-    
+
     const header = JSON.parse(pieces[0]);
-    
+
     const { host, path } = url.parse(header.dsn);
     if (host !== sentryHost) {
       throw new Error(`invalid host: ${host}`);
     }
-    
+
     const projectId = path.endsWith("/") ? path.slice(0, -1) : path;
     if (!knownProjectIds.includes(projectId)) {
       throw new Error(`invalid project id: ${projectId}`);
     }
-    
+
     const url = `https://${sentryHost}/api/${projectId}/envelope/`;
-    return await axios.post(url, envelope);
+    const response = await fetch(url, {
+      method: "POST",
+      body: envelope,
+    });
+    return response.json();
   } catch (e) {
     captureException(e);
     return res.status(400).json({ status: "invalid request" });
