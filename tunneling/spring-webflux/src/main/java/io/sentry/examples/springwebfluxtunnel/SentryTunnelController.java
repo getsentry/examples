f721728ad58b74bb388507b32eeb9177a415c0be
Robert Gründler <robert@dubture.com>
2022-07-17 09:23:23 +0200

    fix response status

--- tunneling/spring-webflux/src/main/java/io/sentry/examples/springwebfluxtunnel/SentryTunnelController.java
+++ tunneling/spring-webflux/src/main/java/io/sentry/examples/springwebfluxtunnel/SentryTunnelController.java
@@ -1,14 +1,19 @@
 package io.sentry.examples.springwebfluxtunnel;
 
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,13 +23,32 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Map;
 import java.util.Set;
 
+import io.netty.handler.logging.LogLevel;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 import reactor.util.retry.Retry;
 
 /**
+ * To add this controller to your app, simply expose it as a bean:
+ *
+ * <pre>
+ *
+ *  {@literal @}Bean
+ *   public SentryTunnelController sentryTunnelController(
+ *     {@literal @}Value("${sentry.tunnel-path}") String tunnelPath,
+ *     {@literal @}Value("${sentry.url}") String sentryUrl,
+ *     {@literal @}Value("#{'${sentry.allowed-project-ids}'.split(',')}") Set<Integer> allowedProjectIds
+ *   ) {
+ *     return new SentryTunnelController(tunnelPath, sentryUrl, allowedProjectIds);
+ *   }
+ *
+ * </pre>
+ *
  * When adding this controller to your application, you will probably need to adjust the security
  * configuration:
  *
@@ -58,40 +82,109 @@ public class SentryTunnelController {
   private static final long MAX_ENVELOPE_SIZE = 1024 * 1024 * 2;
 
   public static final Retry SENTRY_RETRY = Retry
-    .backoff(60, Duration.ofMillis(1000))
-    .jitter(0.2)
-    .filter(
-      throwable ->
-        throwable instanceof IOException || throwable instanceof WebClientResponseException
-    )
-    .doBeforeRetry(
-      retrySignal -> log.info("Retrying sentry request... {}", retrySignal.getClass())
-    );
+      .backoff(60, Duration.ofMillis(1000))
+      .jitter(0.2)
+      .filter(
+          throwable -> {
+            boolean isRetryable =
+                throwable instanceof IOException || throwable instanceof WebClientResponseException;
+
+            if (throwable instanceof WebClientResponseException) {
+              WebClientResponseException responseException = (WebClientResponseException) throwable;
+              log.debug("Sentry response body: {}", responseException.getResponseBodyAsString());
+              responseException
+                  .getHeaders()
+                  .toSingleValueMap()
+                  .forEach(
+                      (key, value) -> log.trace("Response header: {}: {}", key, value)
+                  );
+              if (
+                  responseException.getStatusCode().is4xxClientError() ||
+                  responseException.getStatusCode().is3xxRedirection()
+              ) {
+                log.debug(
+                    "Sentry request not retryable, response code is {}",
+                    responseException.getRawStatusCode()
+                );
+                return false;
+              }
+            }
+
+            return isRetryable;
+          }
+      )
+      .doBeforeRetry(
+          retrySignal -> {
+            String failureString = null;
+            Throwable failure = retrySignal.failure();
+
+            if (failure != null) {
+              Throwable rootCause = ExceptionUtils.getRootCause(failure);
+              if (rootCause != null) {
+                failureString = rootCause.getMessage();
+              }
+            }
+
+            if (failureString == null) {
+              failureString = "unknown";
+            }
+
+            log.info(
+                "Retry # {} sentry request... {}, failure was {}",
+                retrySignal.totalRetries(),
+                retrySignal.getClass(),
+                failureString
+            );
+          }
+      );
 
   public SentryTunnelController(
-    @Value("${sentry.tunnel-path}") String tunnelPath,
-    @Value("${sentry.url}") String sentryUrl,
-    @Value("#{'${sentry.allowed-project-ids}'.split(',')}") Set<Integer> allowedProjectIds
+      @Value("${sentry.tunnel-path}") String tunnelPath,
+      @Value("${sentry.url}") String sentryUrl,
+      @Value("#{'${sentry.allowed-project-ids}'.split(',')}") Set<Integer> allowedProjectIds
   ) {
-    log.info("Creating a sentry tunnel controller with tunnel-path {}", tunnelPath);
+    log.info(
+        "Creating a sentry tunnel controller with tunnel-path {} and allowed-project-ids {}",
+        tunnelPath,
+        allowedProjectIds
+    );
 
     final int size = 16 * 1024 * 1024;
     final ExchangeStrategies strategies = ExchangeStrategies
-      .builder()
-      .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
-      .build();
-    this.client = WebClient.builder().baseUrl(sentryUrl).exchangeStrategies(strategies).build();
+        .builder()
+        .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+        .build();
+
+    HttpClient httpClient = HttpClient
+        .create()
+        .wiretap(
+            "reactor.netty.http.client.HttpClient",
+            LogLevel.DEBUG,
+            AdvancedByteBufFormat.TEXTUAL
+        );
+
+    this.client =
+        WebClient
+            .builder()
+            .baseUrl(sentryUrl)
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .exchangeStrategies(strategies)
+            .build();
+
     this.objectMapper = new ObjectMapper();
     this.allowedProjectIds = allowedProjectIds;
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
   @PostMapping("${sentry.tunnel-path}")
-  public Mono<ResponseEntity<Void>> tunnel(@RequestBody byte[] envelope) {
+  public Mono<ResponseEntity<Object>> tunnel(
+      @RequestBody byte[] envelope,
+      @RequestHeader Map<String, String> headers
+  ) {
     try {
       if (envelope.length > MAX_ENVELOPE_SIZE) {
         return Mono.error(
-          () -> new IllegalArgumentException("Envelope is larger than " + MAX_ENVELOPE_SIZE)
+            () -> new IllegalArgumentException("Envelope is larger than " + MAX_ENVELOPE_SIZE)
         );
       }
 
@@ -111,7 +204,7 @@ public class SentryTunnelController {
       if (dsnNode == null) {
         log.info("DSN not present in header {}", envelopeAsString);
         return Mono.error(
-          () -> new IllegalArgumentException("DNS node not present in sentry envelope")
+            () -> new IllegalArgumentException("DNS node not present in sentry envelope")
         );
       }
 
@@ -123,27 +216,55 @@ public class SentryTunnelController {
       }
 
       URI uri = URI.create(dsn);
-      Integer projectId = Integer.parseInt(uri.getPath().replaceAll("/", ""));
+      Integer projectId = Integer.parseInt(
+          StringUtils.removeEnd(uri.getPath(), "/").replace("/", "")
+      );
 
       if (!allowedProjectIds.contains(projectId)) {
         return Mono.error(() -> new IllegalArgumentException("ProjectID not allowed " + projectId));
       }
 
-      log.info("Forwarding with dsn {}", dsn);
+      log.debug("Forwarding with dsn {}", dsn);
 
       return client
-        .post()
-        .uri("/api/{projectId}/envelope/", projectId)
-        .header(HttpHeaders.CONTENT_TYPE, "application/x-sentry-envelope")
-        .bodyValue(envelope)
-        .retrieve()
-        .toBodilessEntity()
-        .retryWhen(SENTRY_RETRY);
+          .post()
+          .uri("/api/{projectId}/envelope/", projectId)
+          .headers(
+              httpHeaders -> {
+                headers.forEach(
+                    (headerName, headerValue) -> {
+                      if (
+                          HttpHeaders.REFERER.equals(headerName) || HttpHeaders.ORIGIN.equals(headerName)
+                      ) {
+                        log.trace("Forwarding header {} with value {}", headerName, headerValue);
+                        httpHeaders.add(headerName, headerValue);
+                      } else {
+                        log.trace("Not Forwarding header {} with value {}", headerName, headerValue);
+                      }
+                    }
+                );
+              }
+          )
+          .header(HttpHeaders.CONTENT_TYPE, "application/x-sentry-envelope")
+          .bodyValue(envelope)
+          .retrieve()
+          .toBodilessEntity()
+          .retryWhen(SENTRY_RETRY)
+          .flatMap(
+              voidResponseEntity -> {
+                log.trace(
+                    "Sentry request forwarded, response status: {}",
+                    voidResponseEntity.getStatusCode()
+                );
+                return Mono.just(ResponseEntity.status(voidResponseEntity.getStatusCode()).build());
+              }
+          )
+          .doOnError(throwable -> log.warn(throwable.getMessage(), throwable));
     } catch (Exception e) {
       log.info(
-        "Sentry tunnel error: '{}' on envelope '{}'" + e.getMessage(),
-        new String(envelope, StandardCharsets.UTF_8),
-        e
+          "Sentry tunnel error: '{}' on envelope '{}'" + e.getMessage(),
+          new String(envelope, StandardCharsets.UTF_8),
+          e
       );
       return Mono.error(e);
     }
--- tunneling/spring-webflux/src/test/java/io/sentry/examples/springwebfluxtunnel/SentryTunnelControllerTest.java
+++ tunneling/spring-webflux/src/test/java/io/sentry/examples/springwebfluxtunnel/SentryTunnelControllerTest.java
@@ -2,18 +2,25 @@ package io.sentry.examples.springwebfluxtunnel;
 
 import com.google.common.collect.Sets;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.junit.jupiter.MockServerExtension;
 import org.mockserver.junit.jupiter.MockServerSettings;
+import org.mockserver.matchers.Times;
+import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
+import org.mockserver.verify.VerificationTimes;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 import reactor.test.StepVerifier;
 
@@ -29,14 +36,53 @@ class SentryTunnelControllerTest {
     this.client = client;
   }
 
+  @BeforeEach
+  public void setup() {
+    client.reset();
+  }
+
   @Test
   void testForwarding() throws IOException {
     byte[] envelope = new ClassPathResource("/testbody.http").getInputStream().readAllBytes();
 
+    HttpRequest request = request()
+      .withMethod(HttpMethod.POST.name())
+      .withHeader(HttpHeaders.REFERER, "https://google.com")
+      .withPath("/api/4/envelope/")
+      .withBody(envelope);
+
+    client.when(request).respond(HttpResponse.response().withStatusCode(HttpStatus.OK.value()));
+
+    String path = "/telemetry";
+    String url = "http://localhost:9000";
+    HashSet<Integer> allowedProjectIds = Sets.newHashSet(1, 2, 4);
+    SentryTunnelController controller = new SentryTunnelController(path, url, allowedProjectIds);
+
+    Map<String, String> headers = new HashMap<>();
+    headers.put(HttpHeaders.REFERER, "https://google.com");
+    StepVerifier
+      .create(controller.tunnel(envelope, headers))
+      .expectNextMatches(responseEntity -> responseEntity.getStatusCode().is2xxSuccessful())
+      .verifyComplete();
+
+    client.verify(request, VerificationTimes.once());
+  }
+
+  @Test
+  void testRetry() throws IOException {
+    byte[] envelope = new ClassPathResource("/testbody.http").getInputStream().readAllBytes();
+
+    HttpRequest requestMatcher = request()
+      .withMethod(HttpMethod.POST.name())
+      .withPath("/api/4/envelope/")
+      .withBody(envelope);
+
     client
-      .when(
-        request().withMethod(HttpMethod.POST.name()).withPath("/api/4/envelope/").withBody(envelope)
-      )
+      .when(requestMatcher, Times.once())
+      .respond(HttpResponse.response().withStatusCode(HttpStatus.GATEWAY_TIMEOUT.value()));
+
+    client
+      .when(requestMatcher, Times.once())
       .respond(HttpResponse.response().withStatusCode(HttpStatus.OK.value()));
 
     String path = "/telemetry";
@@ -44,6 +90,39 @@ class SentryTunnelControllerTest {
     HashSet<Integer> allowedProjectIds = Sets.newHashSet(1, 2, 4);
     SentryTunnelController controller = new SentryTunnelController(path, url, allowedProjectIds);
 
-    StepVerifier.create(controller.tunnel(envelope)).expectNextCount(1).verifyComplete();
+    Map<String, String> headers = new HashMap<>();
+    StepVerifier
+      .create(controller.tunnel(envelope, headers))
+      .expectNextMatches(responseEntity -> responseEntity.getStatusCode().is2xxSuccessful())
+      .verifyComplete();
+
+    client.verify(requestMatcher, VerificationTimes.exactly(2));
+  }
+
+  @Test
+  void testNoRetryOnClientErrors() throws IOException {
+    byte[] envelope = new ClassPathResource("/testbody.http").getInputStream().readAllBytes();
+
+    HttpRequest requestMatcher = request()
+      .withMethod(HttpMethod.POST.name())
+      .withPath("/api/4/envelope/")
+      .withBody(envelope);
+
+    client
+      .when(requestMatcher, Times.once())
+      .respond(HttpResponse.response().withStatusCode(HttpStatus.BAD_REQUEST.value()));
+
+    String path = "/telemetry";
+    String url = "http://localhost:9000";
+    HashSet<Integer> allowedProjectIds = Sets.newHashSet(1, 2, 4);
+    SentryTunnelController controller = new SentryTunnelController(path, url, allowedProjectIds);
+
+    Map<String, String> headers = new HashMap<>();
+    StepVerifier
+      .create(controller.tunnel(envelope, headers))
+      .expectErrorMessage("400 Bad Request from POST http://localhost:9000/api/4/envelope/")
+      .verify();
+
+    client.verify(requestMatcher, VerificationTimes.exactly(1));
   }
 }
